90a34382bffdcce3d25acfba38411177cb28f89e
Chu-Sheng Juan <cooper.juan@yahoo.com>
2021-03-13 15:07:53 -0500

    first commit

---
+++ 026-npm-command.js
@@ -0,0 +1,9 @@
+// npm - global command, conme with node
+// npm --version
+
+// local dependency - use it only in this particular project
+// mpm i <packageName>
+
+// global dependnency - use it in any project
+// npm install -g <packageName>
+// sudo install -g <packageName> (mac)
